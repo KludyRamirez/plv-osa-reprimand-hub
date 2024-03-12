92ffe8440b099476892530690d1692931a1608a9
KIBENCLYDE <143309770+KIBENCLYDE@users.noreply.github.com>
2024-03-13 00:58:09 +0800

    login test

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/homeBase/Home";
+import Login from "./pages/auth/login/loginBase/LoginBase";
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </Router>
   );
