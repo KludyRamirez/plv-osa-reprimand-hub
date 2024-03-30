4d6b43705667a179d1de282a8b3364c25ae4c0ac
Kludy Ramirez <ramirezkludy23@gmail.com>
2024-03-31 06:18:22 +0800

    [FIX] Tab bar

--- client/src/pages/students/studentsComponents/StudentsFilter.js
+++ client/src/pages/students/studentsComponents/StudentsFilter.js
@@ -5,6 +5,10 @@ const StudentsFilter = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [students, setStudents] = useState([]);
   const [filteredStudents, setFilteredStudents] = useState([]);
+  const [month, setMonth] = useState("");
+  const [day, setDay] = useState("");
+  const [date, setDate] = useState("");
+  const [year, setYear] = useState("");
 
   useEffect(() => {
     const filtered = students.filter((student) => {
@@ -32,28 +36,36 @@ const StudentsFilter = () => {
       <div className="w-100 text-[24px] text-[#303030] font-bold pb-6">
         Students List
       </div>
-      <div className="w-100 h-[160px] bg-blue-100 rounded-[12px] flex flex-col">
-        <div className="w-100 h-[40%] pt-3 pl-4 pr-4 flex justify-start gap-1 border-b-2 border-white">
-          <div className="px-3 h-[52px] hover:border-b-2 border-blue-600 flex justify-center items-center text-[18px]">
+      <div className="w-100 bg-blue-100 rounded-[12px] flex flex-col">
+        <div className="w-100 pt-2 px-4 flex justify-start gap-1">
+          <div className="p-3 hover:border-b-2 border-blue-600 flex justify-center items-center text-[18px]">
             All Students
           </div>
-          <div className="px-3 h-[52px] hover:border-b-2 border-blue-600 flex justify-center items-center text-[18px]">
+          <div className="p-3 hover:border-b-2 border-blue-600 flex justify-center items-center text-[18px]">
             Pending
           </div>
-          <div className="px-3 h-[52px] hover:border-b-2 border-blue-600 flex justify-center items-center text-[18px]">
+          <div className="p-3 hover:border-b-2 border-blue-600 flex justify-center items-center text-[18px]">
             Inactive
           </div>
         </div>
-        <div className="w-100 h-[60%] p-4 ">
+        <div className="w-100 p-4">
           <div className="w-100 flex justify-center gap-3">
             <input
               type="text"
               placeholder="Search by case number, name, email, etc."
-              className="p-3 rounded-[8px] w-[96.6%] focus:outline-[#007bff]"
+              className="p-3 rounded-[8px] w-[100%] focus:outline-[#007bff]"
             />
             <div className="flex justify-center items-center w-[50px] p-3 rounded-[8px] bg-[#007bff] font-semibold text-[white] gap-3">
               <BsSearch className="text-[20px] mt-[-2px]" />
             </div>
+          </div>
+          <div className="w-100 pt-4">
+            <label>Month</label>
+            <select onChange={(e) => setSelectedCategory(e.target.value)}>
+              <option value="All">All</option>
+              <option value="Fruit">Fruit</option>
+              <option value="Vegetable">Vegetable</option>
+            </select>
           </div>
         </div>
       </div>
