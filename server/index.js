a0c97fb57b8c4f078ddfa957724c7a810cb9089f
Kludy Ramirez <ramirezkludy23@gmail.com>
2024-05-20 09:59:47 +0800

    [FIX] serverless state

--- server/index.js
+++ server/index.js
@@ -1,17 +1,12 @@
 const express = require("express");
-const http = require("http");
 const mongoose = require("mongoose");
-// const morgan = require("morgan");
-// const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const fs = require("fs");
 require("dotenv").config();
 
-const PORT = process.env.PORT || process.env.API_PORT;
-
 const app = express();
-
-const server = http.createServer(app);
 
 mongoose
   .connect(process.env.MONGO_URI, {
@@ -19,29 +14,22 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    server.listen(PORT, () => {
-      console.log(`Server is listening on ${PORT}`);
-    });
+    console.log("Database connected!");
   })
   .catch((err) => {
-    console.error("Database is not connected!", err);
+    console.error("Database connection failed!", err);
   });
 
 // Middleware
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (origin === process.env.CLIENT_URI) {
-        callback(null, true);
-      } else {
-        callback(null, true);
-      }
-    },
+    origin: process.env.CLIENT_URI,
     credentials: true,
   })
 );
 
-app.use(express.json({ limit: "2mb" }));
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "2mb" }));
 
 const routeFiles = fs.readdirSync("./routes");
 routeFiles.forEach((file) => {
@@ -50,3 +38,5 @@ routeFiles.forEach((file) => {
     app.use("/api", route);
   }
 });
+
+module.exports = app;
