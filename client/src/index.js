66091413eb6bde1d4df064279da21c879615693b
Kludy Ramirez <ramirezkludy23@gmail.com>
2024-05-22 13:39:02 +0800

    [FIX] Provider store props

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <Provider store={Store}>
+  <Provider store={ReduxStore}>
     <App />
   </Provider>
   // </React.StrictMode>
