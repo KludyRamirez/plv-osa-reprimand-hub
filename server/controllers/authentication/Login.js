a4c91722a66811821c6e85e83ff90473f4d20467
Kludy Ramirez <ramirezkludy23@gmail.com>
2024-10-22 09:42:11 +0800

    fix: server error message: main

--- server/controllers/authentication/Login.js
+++ server/controllers/authentication/Login.js
@@ -1,7 +1,7 @@
-const User = require("../../models/Users");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const Notification = require("../../models/Notifications");
+const User = require('../../models/Users');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const Notification = require('../../models/Notifications');
 
 const login = async (req, res) => {
   try {
@@ -12,14 +12,14 @@ const login = async (req, res) => {
     if (!user) {
       return res
         .status(400)
-        .json({ message: "Invalid credentials. Please try again." });
+        .json({ message: 'Invalid credentials. Please try again.' });
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
       return res
         .status(400)
-        .json({ message: "Invalid credentials. Please try again." });
+        .json({ message: 'Invalid credentials. Please try again.' });
     }
 
     const token = jwt.sign(
@@ -30,7 +30,7 @@ const login = async (req, res) => {
       },
       process.env.ACCESS_TOKEN,
       {
-        expiresIn: "1d",
+        expiresIn: '1d',
       }
     );
 
@@ -42,24 +42,24 @@ const login = async (req, res) => {
       },
       process.env.REFRESH_TOKEN,
       {
-        expiresIn: "1d",
+        expiresIn: '1d',
       }
     );
 
     user.refreshToken = refreshToken;
     await user.save();
 
-    res.cookie("jwt", refreshToken, {
+    res.cookie('jwt', refreshToken, {
       httpOnly: true,
       secure: true,
-      sameSite: "None",
+      sameSite: 'None',
       maxAge: 24 * 60 * 60 * 1000,
     });
 
     await Notification.create({
       userId: user._id,
-      typeOfNotif: "Authentication",
-      actionOfNotif: "Add",
+      typeOfNotif: 'Authentication',
+      actionOfNotif: 'Add',
       message: `${user.userName} has logged in successfully.`,
       createdAt: new Date(),
     });
@@ -73,10 +73,9 @@ const login = async (req, res) => {
       },
     });
   } catch (error) {
-    console.error(error);
     return res
       .status(500)
-      .json({ message: "Something went wrong. Please try again." });
+      .json({ message: 'Something went wrong. Please try again.' });
   }
 };
 
@@ -85,18 +84,18 @@ const handleRefreshToken = async (req, res) => {
     const refreshToken = req.cookies.jwt;
 
     if (!refreshToken) {
-      return res.status(404).send("Error");
+      return res.status(404).send('No refresh token available.');
     }
 
     const user = await User.findOne({ refreshToken: refreshToken });
 
     if (!user) {
-      return res.status(404).send("Unknown user!");
+      return res.status(404).send('Unknown user.');
     }
 
     jwt.verify(refreshToken, process.env.REFRESH_TOKEN, (err, decoded) => {
       if (err || user.userName !== decoded.userName) {
-        return res.status(404).send("Error");
+        return res.status(404).send('Refresh token error.');
       }
 
       const accessToken = jwt.sign(
@@ -106,7 +105,7 @@ const handleRefreshToken = async (req, res) => {
           role: user.role,
         },
         process.env.ACCESS_TOKEN,
-        { expiresIn: "1d" }
+        { expiresIn: '1d' }
       );
 
       return res.status(200).json({
@@ -119,8 +118,9 @@ const handleRefreshToken = async (req, res) => {
       });
     });
   } catch (err) {
-    console.error(err);
-    return res.status(500).send("Server Error");
+    return res
+      .status(500)
+      .json({ message: 'Something went wrong. Please try again.' });
   }
 };
 
@@ -128,35 +128,36 @@ const handleLogout = async (req, res) => {
   const cookies = req.cookies;
 
   try {
-    if (!cookies?.jwt) return res.status(404).send("Error");
+    if (!cookies?.jwt) return res.status(404).send('No cookies available.');
     const refreshToken = cookies.jwt;
 
     const account = await User.findOne({ refreshToken: refreshToken });
 
     if (!account) {
-      res.clearCookie("jwt", {
+      res.clearCookie('jwt', {
         httpOnly: true,
         secure: true,
-        sameSite: "None",
+        sameSite: 'None',
       });
-      return res.status(404).send("Unknown user!");
+      return res.status(404).send('Unknown user.');
     }
 
     await User.updateOne(
       { refreshToken: refreshToken },
-      { $set: { refreshToken: "" } }
+      { $set: { refreshToken: '' } }
     );
 
-    res.clearCookie("jwt", {
+    res.clearCookie('jwt', {
       httpOnly: true,
       secure: true,
-      sameSite: "None",
+      sameSite: 'None',
     });
 
-    res.status(200).json({ message: "Logout successful!" });
+    res.status(200).json({ message: 'Sign out successfully.' });
   } catch (err) {
-    console.error(err);
-    res.status(500).send("Error");
+    return res
+      .status(500)
+      .json({ message: 'Something went wrong. Please try again.' });
   }
 };
 
